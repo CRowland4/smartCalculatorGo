786bb6399d177e68c5ae05ec2f90458168ff9856
Caleb Rowland <cjrowland4@gmail.com>
2024-01-20 10:03:49 -0600

    Commit stage 4 of 5

--- main.go
+++ main.go
@@ -6,45 +6,113 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 func main() {
-	var nums []int
+	var result int
 	for {
-		input := readLine()
-		switch strings.TrimSpace(input) {
+		line := readLine()
+		switch strings.TrimSpace(line) {
 		case "":
 			continue
 		case "/exit":
 			fmt.Println("Bye!")
 			return
 		case "/help":
-			fmt.Println("The program calculates the sum of numbers")
+			fmt.Println("The program calculates simple math operations using + and -")
 			continue
 		default:
-			nums = parseNums(input)
+			result = calculateResult(line)
 		}
 
-		fmt.Println(sum(nums))
+		fmt.Println(result)
 	}
 }
 
-func sum(nums []int) (result int) {
-	for _, num := range nums {
-		result += num
+func calculateResult(input string) (result int) {
+	for input != "" {
+		var digits []rune
+		var nonDigits []rune
+
+		input, nonDigits = consumeNonDigits(input)
+		input, digits = consumeDigits(input)
+		result = newResult(result, getNumFromRunes(digits), getSignFromRunes(nonDigits))
 	}
 
 	return result
 }
 
-func parseNums(input string) (nums []int) {
-	stringNums := strings.Split(input, " ")
-	for _, num := range stringNums {
-		integer, _ := strconv.Atoi(num)
-		nums = append(nums, integer)
+func consumeNonDigits(input string) (newInput string, nonDigits []rune) {
+	newInput = input
+	for _, char := range newInput {
+		if unicode.IsDigit(char) {
+			return newInput, nonDigits
+		}
+
+		nonDigits = append(nonDigits, char)
+		newInput = strings.Replace(newInput, string(char), "", 1)
 	}
 
-	return nums
+	return newInput, nonDigits
+}
+
+func consumeDigits(input string) (newInput string, digits []rune) {
+	newInput = input
+	for _, char := range newInput {
+		if !unicode.IsDigit(char) {
+			return newInput, digits
+		}
+
+		digits = append(digits, char)
+		newInput = strings.Replace(newInput, string(char), "", 1)
+	}
+
+	return newInput, digits
+}
+
+func getSignFromRunes(nonDigitRunes []rune) (sign rune) {
+	sign = '+'
+	for _, char := range nonDigitRunes {
+		switch char {
+		case '-':
+			sign = flipSign(sign)
+		default:
+			continue
+		}
+	}
+
+	return sign
+}
+
+func flipSign(sign rune) (flippedSign rune) {
+	switch sign {
+	case '+':
+		return '-'
+	default:
+		return '+'
+	}
+}
+
+func getNumFromRunes(digitRunes []rune) (num int) {
+	stringNum := ""
+	for _, digitRune := range digitRunes {
+		stringNum += string(digitRune)
+	}
+
+	num, _ = strconv.Atoi(stringNum)
+	return num
+}
+
+func newResult(result, num int, sign rune) (newResult int) {
+	switch sign {
+	case '+':
+		return result + num
+	case '-':
+		return result - num
+	default:
+		return 0
+	}
 }
 
 func readLine() (line string) {
